ef5aa91541861ecb6f4be2b844cd17e8d749b96e
Francesco Faraone <francesco.faraone@softwareone.com>
2024-07-08 11:25:37 +0200

    227554 notify 3yc processing exceptions in teams channel

--- adobe_vipm/flows/benefits.py
+++ adobe_vipm/flows/benefits.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 from urllib.parse import urljoin
 
 from django.conf import settings
@@ -34,7 +35,7 @@ from adobe_vipm.flows.utils import (
     get_company_name,
     get_ordering_parameter,
 )
-from adobe_vipm.notifications import Button, send_warning
+from adobe_vipm.notifications import Button, send_exception, send_warning
 
 logger = logging.getLogger(__name__)
 
@@ -44,90 +45,100 @@ def check_3yc_commitment_request(mpt_client, is_recommitment=False):
     agreements = get_agreements_by_3yc_commitment_request_status(
         mpt_client, is_recommitment=is_recommitment
     )
+    request_type_title = "commitment" if not is_recommitment else "recommitment"
     for agreement in agreements:
-        authorization_id = agreement["authorization"]["id"]
-        customer_id = get_adobe_customer_id(agreement)
-        customer = adobe_client.get_customer(
-            authorization_id,
-            customer_id,
-        )
-
-        request_info = get_3yc_commitment_request(
-            customer, is_recommitment=is_recommitment
-        )
-
-        status_param_ext_id = (
-            PARAM_3YC_COMMITMENT_REQUEST_STATUS
-            if not is_recommitment
-            else PARAM_3YC_RECOMMITMENT_REQUEST_STATUS
-        )
-        request_type_param_ext_id = (
-            PARAM_3YC if not is_recommitment else PARAM_3YC_RECOMMITMENT
-        )
-        request_type_param_phase = (
-            PARAM_PHASE_ORDERING if not is_recommitment else PARAM_PHASE_FULFILLMENT
-        )
-
-        parameters = {
-            PARAM_PHASE_FULFILLMENT: [
-                {
-                    "externalId": status_param_ext_id,
-                    "value": request_info["status"],
-                },
-            ]
-        }
-
-        commitment_info = get_3yc_commitment(customer)
-        if commitment_info:
-            parameters.setdefault(request_type_param_phase, [])
-            parameters[request_type_param_phase].append(
-                {"externalId": request_type_param_ext_id, "value": None},
-            )
-            parameters[PARAM_PHASE_FULFILLMENT].extend(
-                [
-                    {
-                        "externalId": PARAM_3YC_ENROLL_STATUS,
-                        "value": commitment_info["status"],
-                    },
-                    {
-                        "externalId": PARAM_3YC_START_DATE,
-                        "value": commitment_info["startDate"],
-                    },
-                    {
-                        "externalId": PARAM_3YC_END_DATE,
-                        "value": commitment_info["endDate"],
-                    },
-                ],
+        try:
+            authorization_id = agreement["authorization"]["id"]
+            customer_id = get_adobe_customer_id(agreement)
+            customer = adobe_client.get_customer(
+                authorization_id,
+                customer_id,
             )
 
-        update_agreement(
-            mpt_client,
-            agreement["id"],
-            parameters=parameters,
-        )
-        status = request_info["status"]
-        if status in (
-            STATUS_3YC_DECLINED,
-            STATUS_3YC_EXPIRED,
-            STATUS_3YC_NONCOMPLIANT,
-        ):
-            agreement_link = urljoin(
-                settings.MPT_PORTAL_BASE_URL, f"/commerce/agreements/{agreement['id']}"
+            request_info = get_3yc_commitment_request(
+                customer, is_recommitment=is_recommitment
             )
-            request_type_title = "commitment" if not is_recommitment else "recommitment"
-            send_warning(
-                f"3YC {request_type_title.capitalize()} Request {status.capitalize()}",
-                f"The 3-year {request_type_title} request for agreement {agreement['id']} "
-                f"**{agreement['name']}** of the customer **{get_company_name(agreement)}** "
-                f"has been denied: {status}.\n\n"
-                "To request the 3YC again, as a Vendor user, "
-                "modify the Agreement and mark the 3-year "
-                f"{request_type_title} {request_type_param_phase} parameter checkbox again.",
-                button=Button(f"Open {agreement["id"]}", agreement_link),
+
+            status_param_ext_id = (
+                PARAM_3YC_COMMITMENT_REQUEST_STATUS
+                if not is_recommitment
+                else PARAM_3YC_RECOMMITMENT_REQUEST_STATUS
+            )
+            request_type_param_ext_id = (
+                PARAM_3YC if not is_recommitment else PARAM_3YC_RECOMMITMENT
+            )
+            request_type_param_phase = (
+                PARAM_PHASE_ORDERING if not is_recommitment else PARAM_PHASE_FULFILLMENT
+            )
+
+            parameters = {
+                PARAM_PHASE_FULFILLMENT: [
+                    {
+                        "externalId": status_param_ext_id,
+                        "value": request_info["status"],
+                    },
+                ]
+            }
+            logger.info(f"3YC request for agreement {agreement['id']} is {request_info['status']}")
+            commitment_info = get_3yc_commitment(customer)
+            if commitment_info:
+                parameters.setdefault(request_type_param_phase, [])
+                parameters[request_type_param_phase].append(
+                    {"externalId": request_type_param_ext_id, "value": None},
+                )
+                parameters[PARAM_PHASE_FULFILLMENT].extend(
+                    [
+                        {
+                            "externalId": PARAM_3YC_ENROLL_STATUS,
+                            "value": commitment_info["status"],
+                        },
+                        {
+                            "externalId": PARAM_3YC_START_DATE,
+                            "value": commitment_info["startDate"],
+                        },
+                        {
+                            "externalId": PARAM_3YC_END_DATE,
+                            "value": commitment_info["endDate"],
+                        },
+                    ],
+                )
+
+            update_agreement(
+                mpt_client,
+                agreement["id"],
+                parameters=parameters,
+            )
+            status = request_info["status"]
+            if status in (
+                STATUS_3YC_DECLINED,
+                STATUS_3YC_EXPIRED,
+                STATUS_3YC_NONCOMPLIANT,
+            ):
+                agreement_link = urljoin(
+                    settings.MPT_PORTAL_BASE_URL, f"/commerce/agreements/{agreement['id']}"
+                )
+                send_warning(
+                    f"3YC {request_type_title.capitalize()} Request {status.capitalize()}",
+                    f"The 3-year {request_type_title} request for agreement {agreement['id']} "
+                    f"**{agreement['name']}** of the customer **{get_company_name(agreement)}** "
+                    f"has been denied: {status}.\n\n"
+                    "To request the 3YC again, as a Vendor user, "
+                    "modify the Agreement and mark the 3-year "
+                    f"{request_type_title} {request_type_param_phase} parameter checkbox again.",
+                    button=Button(f"Open {agreement["id"]}", agreement_link),
+                )
+        except Exception:
+            logger.exception(
+                f"An exception has been raised checking 3YC request for {agreement['id']}",
+            )
+            send_exception(
+                f"3YC {request_type_title.capitalize()} Request exception for {agreement['id']}",
+                traceback.format_exc()
             )
 
 
 def resubmit_3yc_commitment_request(mpt_client, is_recommitment=False):
+    request_type_title = "Commitment" if not is_recommitment else "Recommitment"
     status_param_ext_id = (
         PARAM_3YC_COMMITMENT_REQUEST_STATUS
         if not is_recommitment
@@ -138,75 +149,91 @@ def resubmit_3yc_commitment_request(mpt_client, is_recommitment=False):
         mpt_client, is_recommitment=is_recommitment
     )
     for agreement in agreements:
-        authorization_id = agreement["authorization"]["id"]
-        customer_id = get_adobe_customer_id(agreement)
+        try:
+            authorization_id = agreement["authorization"]["id"]
+            customer_id = get_adobe_customer_id(agreement)
 
-        commitment_request = {
-            PARAM_3YC_CONSUMABLES: get_ordering_parameter(
-                agreement, PARAM_3YC_CONSUMABLES
-            ).get("value"),
-            PARAM_3YC_LICENSES: get_ordering_parameter(
-                agreement, PARAM_3YC_LICENSES
-            ).get("value"),
-        }
+            commitment_request = {
+                PARAM_3YC_CONSUMABLES: get_ordering_parameter(
+                    agreement, PARAM_3YC_CONSUMABLES
+                ).get("value"),
+                PARAM_3YC_LICENSES: get_ordering_parameter(
+                    agreement, PARAM_3YC_LICENSES
+                ).get("value"),
+            }
+            customer = adobe_client.create_3yc_request(
+                authorization_id,
+                customer_id,
+                commitment_request,
+                is_recommitment=is_recommitment,
+            )
 
-        customer = adobe_client.create_3yc_request(
-            authorization_id,
-            customer_id,
-            commitment_request,
-            is_recommitment=is_recommitment,
-        )
+            commitment_info = get_3yc_commitment_request(
+                customer, is_recommitment=is_recommitment
+            )
+            status = commitment_info["status"]
+            parameters = {
+                PARAM_PHASE_FULFILLMENT: [
+                    {"externalId": status_param_ext_id, "value": status},
+                ]
+            }
 
-        commitment_info = get_3yc_commitment_request(
-            customer, is_recommitment=is_recommitment
-        )
-        status = commitment_info["status"]
-        parameters = {
-            PARAM_PHASE_FULFILLMENT: [
-                {"externalId": status_param_ext_id, "value": status},
-            ]
-        }
-
-        update_agreement(
-            mpt_client,
-            agreement["id"],
-            parameters=parameters,
-        )
-
+            update_agreement(
+                mpt_client,
+                agreement["id"],
+                parameters=parameters,
+            )
+        except Exception:
+            logger.exception(
+                f"An exception has been raised checking 3YC request for {agreement['id']}",
+            )
+            send_exception(
+                f"3YC {request_type_title} Request exception for {agreement['id']}",
+                traceback.format_exc()
+            )
 
 def submit_3yc_recommitment_request(mpt_client):
     adobe_client = get_adobe_client()
     agreements = get_agreements_for_3yc_recommitment(mpt_client)
     for agreement in agreements:
-        authorization_id = agreement["authorization"]["id"]
-        customer_id = get_adobe_customer_id(agreement)
+        try:
+            authorization_id = agreement["authorization"]["id"]
+            customer_id = get_adobe_customer_id(agreement)
 
-        commitment_request = {
-            PARAM_3YC_CONSUMABLES: get_ordering_parameter(
-                agreement, PARAM_3YC_CONSUMABLES
-            ).get("value"),
-            PARAM_3YC_LICENSES: get_ordering_parameter(
-                agreement, PARAM_3YC_LICENSES
-            ).get("value"),
-        }
+            commitment_request = {
+                PARAM_3YC_CONSUMABLES: get_ordering_parameter(
+                    agreement, PARAM_3YC_CONSUMABLES
+                ).get("value"),
+                PARAM_3YC_LICENSES: get_ordering_parameter(
+                    agreement, PARAM_3YC_LICENSES
+                ).get("value"),
+            }
 
-        customer = adobe_client.create_3yc_request(
-            authorization_id,
-            customer_id,
-            commitment_request,
-            is_recommitment=True,
-        )
+            customer = adobe_client.create_3yc_request(
+                authorization_id,
+                customer_id,
+                commitment_request,
+                is_recommitment=True,
+            )
 
-        commitment_info = get_3yc_commitment_request(customer, is_recommitment=True)
-        status = commitment_info["status"]
-        parameters = {
-            PARAM_PHASE_FULFILLMENT: [
-                {"externalId": "3YCRecommitmentRequestStatus", "value": status},
-            ]
-        }
+            commitment_info = get_3yc_commitment_request(customer, is_recommitment=True)
+            status = commitment_info["status"]
+            parameters = {
+                PARAM_PHASE_FULFILLMENT: [
+                    {"externalId": "3YCRecommitmentRequestStatus", "value": status},
+                ]
+            }
 
-        update_agreement(
-            mpt_client,
-            agreement["id"],
-            parameters=parameters,
-        )
+            update_agreement(
+                mpt_client,
+                agreement["id"],
+                parameters=parameters,
+            )
+        except Exception:
+            logger.exception(
+                f"An exception has been raised checking 3YC request for {agreement['id']}",
+            )
+            send_exception(
+                f"3YC Recommitment Request exception for {agreement['id']}",
+                traceback.format_exc()
+            )
