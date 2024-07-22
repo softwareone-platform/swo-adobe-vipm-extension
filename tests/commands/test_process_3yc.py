e8518dcca5dc91fa746de0c87ce830096306f0cd
Francesco Faraone <francesco.faraone@softwareone.com>
2024-07-22 08:07:36 +0000

    227861 disable submit/resubmit of commitment/recommitment requests

--- tests/commands/test_process_3yc.py
+++ tests/commands/test_process_3yc.py
@@ -8,12 +8,6 @@ def test_process_sync_agreements(mocker):
         return_value=mocked_client,
     )
 
-    mocked_submit = mocker.patch(
-        "adobe_vipm.management.commands.process_3yc.submit_3yc_recommitment_request"
-    )
-    mocked_resubmit = mocker.patch(
-        "adobe_vipm.management.commands.process_3yc.resubmit_3yc_commitment_request"
-    )
     mocked_check = mocker.patch(
         "adobe_vipm.management.commands.process_3yc.check_3yc_commitment_request"
     )
@@ -24,10 +18,3 @@ def test_process_sync_agreements(mocker):
     assert mocked_check.mock_calls[0].kwargs == {}
     assert mocked_check.mock_calls[1].args == (mocked_client,)
     assert mocked_check.mock_calls[1].kwargs == {"is_recommitment": True}
-
-    mocked_submit.assert_called_once_with(mocked_client)
-
-    assert mocked_resubmit.mock_calls[0].args == (mocked_client,)
-    assert mocked_resubmit.mock_calls[0].kwargs == {}
-    assert mocked_resubmit.mock_calls[1].args == (mocked_client,)
-    assert mocked_resubmit.mock_calls[1].kwargs == {"is_recommitment": True}
