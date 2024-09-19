e931f40c1873555b435bec20349be7b83a165abd
Francesco Faraone <francesco.faraone@softwareone.com>
2024-09-19 10:04:58 +0000

    MPT-2884 handle first order case only on validation of downsizes

--- adobe_vipm/flows/validation/change.py
+++ adobe_vipm/flows/validation/change.py
@@ -6,6 +6,7 @@ from operator import attrgetter
 from adobe_vipm.adobe.client import get_adobe_client
 from adobe_vipm.flows.constants import (
     ERR_INVALID_DOWNSIZE_QUANTITY,
+    ERR_INVALID_ITEM_DOWNSIZE_FIRST_PO,
     ERR_INVALID_ITEM_DOWNSIZE_QUANTITY,
     ERR_INVALID_ITEM_DOWNSIZE_QUANTITY_ANY_COMBINATION,
 )
@@ -19,6 +20,14 @@ logger = logging.getLogger(__name__)
 
 
 class ValidateDownsizes(Step):
+    @staticmethod
+    def get_returnable_by_quantity_map(returnable_orders):
+        returnable_by_quantity = {}
+        for r in range(len(returnable_orders), 0, -1):
+            for sub in itertools.combinations(returnable_orders, r):
+                returnable_by_quantity[sum([x.quantity for x in sub])] = sub
+        return returnable_by_quantity
+
     def __call__(self, client, context, next_step):
         adobe_client = get_adobe_client()
         errors = []
@@ -37,12 +46,10 @@ class ValidateDownsizes(Step):
             if not returnable_orders:
                 continue
 
-            returnable_by_quantity = {}
-            for r in range(len(returnable_orders), 0, -1):
-                for sub in itertools.combinations(returnable_orders, r):
-                    returnable_by_quantity[sum([x.quantity for x in sub])] = sub
+            returnable_by_quantity = self.get_returnable_by_quantity_map(returnable_orders)
 
             delta = line["oldQuantity"] - line["quantity"]
+
             if delta not in returnable_by_quantity:
                 end_of_cancellation_window = max(
                     datetime.fromisoformat(roi.order["creationDate"]).date()
@@ -51,10 +58,22 @@ class ValidateDownsizes(Step):
                 end_of_cancellation_window = min(
                     end_of_cancellation_window, last_two_weeks
                 )
+
                 quantities = [
                     str(roi.quantity)
                     for roi in sorted(returnable_orders, key=attrgetter("quantity"))
+                    if roi.quantity != line["oldQuantity"]
                 ]
+                if len(quantities) == 0:
+                    message = ERR_INVALID_ITEM_DOWNSIZE_FIRST_PO.format(
+                        item=line["item"]["name"],
+                        delta=delta,
+                        quantity=line["quantity"],
+                    )
+                    errors.append(message)
+                    context.validation_succeeded = False
+                    continue
+
                 message = ERR_INVALID_ITEM_DOWNSIZE_QUANTITY.format(
                     item=line["item"]["name"],
                     delta=delta,
@@ -74,8 +93,8 @@ class ValidateDownsizes(Step):
                 context.order,
                 ERR_INVALID_DOWNSIZE_QUANTITY.to_dict(messages="\n".join(errors)),
             )
-        else:
-            next_step(client, context)
+            return
+        next_step(client, context)
 
 
 def validate_change_order(client, order):
