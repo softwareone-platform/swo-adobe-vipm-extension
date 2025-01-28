a4d96d4158030d8279ed8f38e80df8c7f8963e5a
Ruben Sebrango <ruben.sebrango@softwareone.com>
2025-01-28 12:18:17 +0100

    [HF] MPT-6502 Enable deployment subscriptions auto-renew

--- tests/flows/test_global_customer.py
+++ tests/flows/test_global_customer.py
@@ -7,9 +7,9 @@ def test_check_gc_agreement_deployments_no_licensee(mocker, mpt_client, settings
     settings.EXTENSION_CONFIG = {
         "AIRTABLE_API_TOKEN": "api_key",
         "AIRTABLE_BASES": {"PRD-1111-1111": "base_id"},
-        "PRODUCT_SEGMENT": {"PRD-1111-1111": "COM","PRD-2222-2222": "GOV"},
+        "PRODUCT_SEGMENT": {"PRD-1111-1111": "COM", "PRD-2222-2222": "GOV"},
     }
-    settings.MPT_PRODUCTS_IDS = ["PRD-1111-1111","PRD-2222-2222"]
+    settings.MPT_PRODUCTS_IDS = ["PRD-1111-1111", "PRD-2222-2222"]
     mocked_adobe_client = mocker.MagicMock()
     mocker.patch(
         "adobe_vipm.flows.global_customer.get_adobe_client",
@@ -967,6 +967,97 @@ def test_check_gc_agreement_deployments_create_agreement_subscription_error(
     mocked_get_licensee.assert_called_once()
     mocked_adobe_client.get_customer.assert_called_once()
     mocked_adobe_client.get_customer_deployments.assert_called_once()
+
+    mocked_update_agreement.assert_called_once()
+    mocked_get_product_items_by_skus.assert_called()
+    mocked_get_listing_by_id.assert_called_once()
+    mocked_get_subscription_by_external_id.assert_called_once()
+    mocked_create_agreement_subscription.assert_called_once()
+
+
+def test_check_gc_agreement_deployments_create_agreement_subscription_enable_auto_renew(
+    mocker,
+    mpt_client,
+    settings,
+    mpt_error_factory,
+    adobe_customer_factory,
+    adobe_subscription_factory,
+    items_factory,
+):
+    settings.EXTENSION_CONFIG = {
+        "AIRTABLE_API_TOKEN": "api_key",
+        "AIRTABLE_BASES": {"PRD-1111-1111": "base_id"},
+        "PRODUCT_SEGMENT": {"PRD-1111-1111": "COM"},
+    }
+    settings.MPT_API_TOKEN_OPERATIONS = "operations_api_key"
+
+    mocked_adobe_client = mocker.MagicMock()
+    mocker.patch(
+        "adobe_vipm.flows.global_customer.get_adobe_client",
+        return_value=mocked_adobe_client,
+    )
+    mocked_gc_agreement_deployments_model = mocker.MagicMock()
+
+    mocked_get_licensee = mocker.patch(
+        "adobe_vipm.flows.global_customer.get_licensee",
+        return_value=mocker.MagicMock(),
+    )
+
+    mocked_update_agreement = mocker.patch(
+        "adobe_vipm.flows.global_customer.update_agreement",
+        return_value=mocker.MagicMock(),
+    )
+
+    mocked_get_listing_by_id = mocker.patch(
+        "adobe_vipm.flows.global_customer.get_listing_by_id",
+        return_value=mocker.MagicMock(),
+    )
+    mocked_get_subscription_by_external_id = mocker.patch(
+        "adobe_vipm.flows.global_customer.get_agreement_subscription_by_external_id",
+        return_value=[],
+    )
+
+    mocked_create_agreement_subscription = mocker.patch(
+        "adobe_vipm.flows.global_customer.create_agreement_subscription",
+        return_value=mocker.MagicMock(),
+    )
+
+    mocker.patch(
+        "adobe_vipm.flows.airtable.get_gc_agreement_deployment_model",
+        return_value=mocked_gc_agreement_deployments_model,
+    )
+    product_items = items_factory()
+    mocked_get_product_items_by_skus = mocker.patch(
+        "adobe_vipm.flows.global_customer.get_product_items_by_skus",
+        return_value=product_items,
+    )
+    adobe_customer = adobe_customer_factory()
+    mocked_adobe_client.get_customer.return_value = adobe_customer
+    mocked_adobe_client.get_customer_deployments.return_value = mocker.MagicMock()
+    mocked_adobe_client.get_subscriptions.return_value = {
+        "items": [
+            adobe_subscription_factory(
+                deployment_id="deployment_id", autorenewal_enabled=False
+            )
+        ]
+    }
+
+    gc_agreement_deployments = mocker.MagicMock()
+    gc_agreement_deployments.licensee_id = "licensee_id"
+    gc_agreement_deployments.authorization_id = "authorization_id"
+    gc_agreement_deployments.price_list_id = "price_list_id"
+    gc_agreement_deployments.listing_id = "listing_id"
+    gc_agreement_deployments.agreement_id = "agreement_id"
+    gc_agreement_deployments.deployment_id = "deployment_id"
+
+    mocked_gc_agreement_deployments_model.all.return_value = [gc_agreement_deployments]
+
+    check_gc_agreement_deployments()
+    mocked_gc_agreement_deployments_model.all.assert_called_once()
+    mocked_get_licensee.assert_called_once()
+    mocked_adobe_client.get_customer.assert_called_once()
+    mocked_adobe_client.get_customer_deployments.assert_called_once()
+    mocked_adobe_client.update_subscription.assert_called_once()
 
     mocked_update_agreement.assert_called_once()
     mocked_get_product_items_by_skus.assert_called()
