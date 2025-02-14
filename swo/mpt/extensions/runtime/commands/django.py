ae495f29420644a3bf3f96006b37f8b3ee07e5e9
Pavel Lonkin <lonkin01@gmail.com>
2025-02-14 16:15:34 +0100

    MPT-4429 Add Django command reporting and fix local run

--- swo/mpt/extensions/runtime/commands/django.py
+++ swo/mpt/extensions/runtime/commands/django.py
@@ -1,5 +1,9 @@
 import click
 
+from contextlib import nullcontext
+
+from opentelemetry import trace
+
 
 @click.command(add_help_option=False, context_settings=dict(ignore_unknown_options=True))
 @click.argument("management_args", nargs=-1, type=click.UNPROCESSED)
@@ -10,5 +14,15 @@ def django(ctx, management_args):
 
     initialize({})
     from django.core.management import execute_from_command_line
+    from django.conf import settings
 
-    execute_from_command_line(argv=[ctx.command_path] + list(management_args))
+    if settings.USE_APPLICATIONINSIGHTS:
+        tracer = trace.get_tracer(__name__)
+        tracer_context = tracer.start_as_current_span(
+            f"Running Django command {management_args[0]}",
+        )
+    else:
+        tracer_context = nullcontext()
+
+    with tracer_context:
+        execute_from_command_line(argv=[ctx.command_path] + list(management_args))
