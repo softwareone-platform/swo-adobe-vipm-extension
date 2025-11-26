4a53396f60640bd1c6b0f569a7e7b7543e7f3ceb
Lukasz Lancucki <lukasz.lancucki@softwareone.com>
2025-11-26 10:04:51 +0000

    fix(sync): improve test for a case when extension is not configured for a product before running a sync

--- tests/flows/test_sync.py
+++ tests/flows/test_sync.py
@@ -3094,10 +3094,13 @@ def test_check_update_airtable_missing_deployments_none(
 
 
 def test_not_syncing_unknown_products(
-        mocker, mock_mpt_client, agreement_factory, mock_get_adobe_client, caplog
+    mock_mpt_client, agreement_factory, mock_get_adobe_client, mock_adobe_client, caplog
 ):
     agreement = agreement_factory()
     agreement["product"]["id"] = "NOT_CONFIGURED_PRODUCT"
+    mock_adobe_client.get_customer.side_effect = AuthorizationNotFoundError(
+        "Authorization with uk/id AUT-9054-3239 not found."
+    )
 
     sync_agreement(mock_mpt_client, agreement, dry_run=False, sync_prices=True)
 
