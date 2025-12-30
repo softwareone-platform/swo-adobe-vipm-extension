a635a6f60998eac9ce9ce8c0b51c574427d5942e
David Franco <david.franco.gutierrez@gmail.com>
2025-12-30 12:03:51 +0100

    MPT-16542 [Monitoring] KeyError - Error synchronizing agreement AGR-8893-0746-1855

--- tests/flows/sync/test_asset.py
+++ tests/flows/sync/test_asset.py
@@ -39,7 +39,49 @@ def test_asset_syncer_sync(
         asset_id,
         parameters={
             "fulfillment": [
+                {"externalId": "lastSyncDate", "value": "2025-06-23"},
                 {"externalId": "usedQuantity", "value": "6"},
+            ]
+        },
+    )
+
+
+@freeze_time("2025-06-23")
+def test_asset_syncer_sync_without_used_quantity(
+    mocker,
+    mock_mpt_client,
+    adobe_subscription_factory,
+    adobe_customer_factory,
+    agreement_factory,
+    assets_factory,
+    lines_factory,
+    mock_mpt_update_asset,
+):
+    asset_id = "AST-1111-2222-3333"
+    mock_asset = assets_factory(asset_id=asset_id, adobe_subscription_id="sub-one-time-id")[0]
+    mock_mpt_get_asset_by_id = mocker.patch(
+        "mpt_extension_sdk.mpt_http.mpt.get_asset_by_id", return_value=mock_asset
+    )
+    mock_lines = lines_factory(external_vendor_id="65304578CA")
+    agreement = agreement_factory(lines=mock_lines, assets=[mock_asset], subscriptions=[])
+    adobe_subscription = adobe_subscription_factory(
+        subscription_id="sub-one-time-id",
+        offer_id="65304578CA01A12",
+    )
+    del adobe_subscription["usedQuantity"]
+    mock_customer = adobe_customer_factory(coterm_date="2025-04-04")
+    assets_syncer = AssetSyncer(
+        mock_mpt_client, agreement["id"], [mock_asset], mock_customer, [adobe_subscription]
+    )
+
+    assets_syncer.sync(dry_run=False)  # act
+
+    mock_mpt_get_asset_by_id.assert_called_once_with(mock_mpt_client, asset_id)
+    mock_mpt_update_asset.assert_called_once_with(
+        mock_mpt_client,
+        asset_id,
+        parameters={
+            "fulfillment": [
                 {"externalId": "lastSyncDate", "value": "2025-06-23"},
             ]
         },
